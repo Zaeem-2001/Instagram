1f0600ba3b93fe04d523ef16bcdda0b8089fc29f
Muhammad Zaeem <muhammad.zaeem@devsinc.com>
2022-06-01 15:30:45 +0500

    Added attributes to Devise
    *  added full name in user model
    
    *  addded username  in user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    root to: 'devise/registrations#new'
+    # root to: 'devise/registrations#new'
+    
+    root to: 'home#index'
   end
 end
