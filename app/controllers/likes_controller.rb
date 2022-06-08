0236558458f8bc0e1da25238aa5a40b7c2dec461
Muhammad Zaeem <muhammad.zaeem@devsinc.com>
2022-06-08 15:32:42 +0500

    Added like module  Added like controller with methods create and destroy

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,35 @@
+class LikesController < ApplicationController
+  before_action :find_post
+  before_action :already_liked?
+  def create
+    if already_liked?
+      flash[:notice] ="you can like more than one"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] ="cannot unlik"
+    else
+      @like=@post.likes.find(params[:id])
+      @like.destroy
+    end
+    redirect_to post_path(@post)
+
+
+  end
+  private
+
+  def find_post
+    @post= Post.find( params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id , post_id: Post.find(params[:post_id])).exists?
+  end
+
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   has_many_attached :images
 
   has_many :comments , dependent: :destroy
-
+  has_many :likes , dependent: :destroy
   delegate :full_name, :username, to: :user
 end
