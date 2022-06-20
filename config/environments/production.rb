022bf3dc9d110f7e1637a9f42740b0a752c87342
Muhammad Zaeem <muhammad.zaeem@devsinc.com>
2022-06-20 15:48:02 +0500

    Added mailer configration to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'muhammad.zaeem@devsinc.com',
+    password: 'yjrgaouioargktvf',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
